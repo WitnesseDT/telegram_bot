ea0dc84d1b6500769d780b2a1a06920327e0f049
WitnesseDT <andrew.malkov1997@icloud.com>
2024-08-06 00:09:55 +0300

    edit

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from telebot import formatting
 from telebot.async_telebot import AsyncTeleBot
 
 import asyncio
-import default_messages, jokes, config, inlines
-
+import default_messages, jokes, config, buttons, complinemt
 
 bot = AsyncTeleBot(config.BOT_TOKEN, parse_mode="HTML")
 
@@ -45,10 +44,42 @@ def any_query(query: types.InlineQuery):
 
 @bot.inline_handler(func=any_query)
 async def any_inline_query(query: types.InlineQuery):
+    joke = jokes.get_jokesi()
+    compliments = complinemt.get_for_women_comlinemts()
+    kb = buttons.kb_joke_handler(joke[1], joke[2], joke[3], joke[4])
+
+    content_dice = types.InputTextMessageContent(
+        message_text=dice
+    )
+
+    content_joke = types.InputTextMessageContent(
+        message_text=formatting.format_text(formatting.hbold("Шутейка: \n"), formatting.hcite(joke[0]),),
+        parse_mode="HTML",
+    )
+
+    content_compliment = types.InputTextMessageContent(
+        message_text=formatting.format_text(formatting.hbold("Коплементарный: \n"), formatting.hcite(compliments)),
+        parse_mode="HTML"
+    )
+
+    result_joke_article = types.InlineQueryResultArticle(
+        id = "joke-id",
+        title = "Шутка минутка",
+        input_message_content = content_joke,
+        thumbnail_url = config.THUMNAIL_PHOTO_JOKE_FOR_INLINE_QUERY,
+        reply_markup = kb
+        
+    ) 
+    result_comliment_article = types.InlineQueryResultArticle(
+        id = "complment-id",
+        title = "Комплиментарный комплимент",
+        input_message_content = content_compliment,
+        thumbnail_url = config.THUMNAIL_PHOTO_COMLIMENT_FOR_INLINE_QUERY
+    )
 
     result = [
-        inlines.result_joke_article,
-        inlines.result_comliment_article,
+        result_joke_article,
+        result_comliment_article,
 
     ]
     await bot.answer_inline_query(
